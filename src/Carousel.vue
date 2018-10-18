2d0effae0f35d5f898de96e24be4398a1f0acb1c
s950329 <s950329@hotmail.com>
2018-10-18 10:39:18 +0800

    add all owl carousel support option

--- src/Carousel.vue
+++ src/Carousel.vue
@@ -1,15 +1,12 @@
 <template>
   <div>
-    <span v-show="showPrev"
-          :id="prevHandler">
+    <span v-show="showPrev" :id="prevHandler">
       <slot name="prev" />
     </span>
-    <div :id="elementHandle"
-         :class="['owl-carousel', 'owl-theme']">
+    <div :id="elementHandle" :class="['owl-carousel', 'owl-theme']">
       <slot />
     </div>
-    <span v-show="showNext"
-          :id="nextHandler">
+    <span v-show="showNext" :id="nextHandler">
       <slot name="next" />
     </span>
   </div>
@@ -40,22 +37,6 @@ export default {
       type: Boolean,
       default: false,
     },
-    nav: {
-      type: Boolean,
-      default: true,
-    },
-    autoplay: {
-      type: Boolean,
-      default: false,
-    },
-    autoplaySpeed: {
-      type: Boolean,
-      default: false,
-    },
-    rewind: {
-      type: Boolean,
-      default: true,
-    },
     mouseDrag: {
       type: Boolean,
       default: true,
@@ -76,17 +57,77 @@ export default {
       type: Number,
       default: 0,
     },
+    merge: {
+      type: Boolean,
+      default: false,
+    },
+    mergeFit: {
+      type: Boolean,
+      default: false,
+    },
     autoWidth: {
       type: Boolean,
       default: false,
     },
-    autoHeight: {
+    startPosition: {
+      type: Number,
+      default: 0,
+    },
+    uRLhashListener: {
       type: Boolean,
       default: false,
+    },
+    nav: {
+      type: Boolean,
+      default: true,
+    },
+    rewind: {
+      type: Boolean,
+      default: true,
+    },
+    navText: {
+      type: Array,
+      default: () => ['next', 'prev'],
+    },
+    navElement: {
+      type: String,
+      default: 'div',
+    },
+    slideBy: {
+      type: [Number, String],
+      default: 1,
+    },
+    slideTransition: {
+      type: String,
+      default: '',
     },
     dots: {
       type: Boolean,
       default: true,
+    },
+    dotsEach: {
+      type: [Number, Boolean],
+      default: false,
+    },
+    dotsData: {
+      type: Boolean,
+      default: false,
+    },
+    lazyLoad: {
+      type: Boolean,
+      default: false,
+    },
+    lazyLoadEager: {
+      type: Number,
+      default: 0,
+    },
+    autoplay: {
+      type: Boolean,
+      default: false,
+    },
+    autoplaySpeed: {
+      type: Boolean,
+      default: false,
     },
     autoplayTimeout: {
       type: Number,
@@ -96,9 +137,81 @@ export default {
       type: Boolean,
       default: false,
     },
+    smartSpeed: {
+      type: Number,
+      default: 250,
+    },
+    fluidSpeed: {
+      type: [Number, Boolean],
+      default: false,
+    },
+    navSpeed: {
+      type: [Number, Boolean],
+      default: false,
+    },
+    dragEndSpeed: {
+      type: [Number, Boolean],
+      default: false,
+    },
+    callbacks: {
+      type: Boolean,
+      default: true,
+    },
     responsive: {
       type: Object,
       default: () => {},
+    },
+    responsiveRefreshRate: {
+      type: Number,
+      default: 200,
+    },
+    video: {
+      type: Boolean,
+      default: false,
+    },
+    videoHeight: {
+      type: [Number, Boolean],
+      default: false,
+    },
+    videoWidth: {
+      type: [Number, Boolean],
+      default: false,
+    },
+    animateOut: {
+      type: [String, Boolean],
+      default: false,
+    },
+    animateIn: {
+      type: [String, Boolean],
+      default: false,
+    },
+    fallbackEasing: {
+      type: String,
+      default: 'swing',
+    },
+    info: {
+      type: Function,
+      default: () => {},
+    },
+    itemElement: {
+      type: String,
+      default: 'div',
+    },
+    stageElement: {
+      type: String,
+      default: 'div',
+    },
+    navContainer: {
+      type: [String, Boolean],
+      default: false,
+    },
+    dotsContainer: {
+      type: [String, Boolean],
+      default: false,
+    },
+    checkVisible: {
+      type: Boolean,
+      default: true,
     },
   },
   data: function() {
@@ -118,21 +231,50 @@ export default {
       margin: this.margin,
       loop: this.loop,
       center: this.center,
-      nav: this.nav,
-      autoplay: this.autoplay,
-      autoplaySpeed: this.autoplaySpeed,
-      rewind: this.rewind,
       mouseDrag: this.mouseDrag,
       touchDrag: this.touchDrag,
       pullDrag: this.pullDrag,
       freeDrag: this.freeDrag,
       stagePadding: this.stagePadding,
+      merge: this.merge,
+      mergeFit: this.mergeFit,
       autoWidth: this.autoWidth,
-      autoHeight: this.autoHeight,
+      startPosition: this.startPosition,
+      uRLhashListener: this.uRLhashListener,
+      nav: this.nav,
+      rewind: this.rewind,
+      navText: this.navText,
+      navElement: this.navElement,
+      slideBy: this.slideBy,
+      slideTransition: this.slideTransition,
       dots: this.dots,
+      dotsEach: this.dotsEach,
+      dotsData: this.dotsData,
+      lazyLoad: this.lazyLoad,
+      lazyLoadEager: this.lazyLoadEager,
+      autoplay: this.autoplay,
+      autoplaySpeed: this.autoplaySpeed,
       autoplayTimeout: this.autoplayTimeout,
       autoplayHoverPause: this.autoplayHoverPause,
+      smartSpeed: this.smartSpeed,
+      fluidSpeed: this.fluidSpeed,
+      navSpeed: this.navSpeed,
+      dragEndSpeed: this.dragEndSpeed,
+      callbacks: this.callbacks,
       responsive: this.responsive,
+      responsiveRefreshRate: this.responsiveRefreshRate,
+      video: this.video,
+      videoHeight: this.videoHeight,
+      videoWidth: this.videoWidth,
+      animateOut: this.animateOut,
+      animateIn: this.animateIn,
+      fallbackEasing: this.fallbackEasing,
+      info: this.info,
+      itemElement: this.itemElement,
+      stageElement: this.stageElement,
+      navContainer: this.navContainer,
+      dotsContainer: this.dotsContainer,
+      checkVisible: this.checkVisible,
     });
 
     $('#' + this.prevHandler).click(function() {
